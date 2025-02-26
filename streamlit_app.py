d6340cb9dd71fab47a083ebee24764dba134439d
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 15:32:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,34 @@
 import streamlit as st
-import pyodbc as db
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
 
 st.title("GOAT L'Oreal PPT Report Automation")
 st.write("Please select the report type")
 with st.sidebar:
     st.write("GOAT L'Oreal Monthly Report")
-    
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-#@st.cache_resource
-#def init_connection():
-    #return 
-conn = db.connect(
-        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
-        + st.secrets["server"]
-        + ";DATABASE="
-        + st.secrets["database"]
-        + ";UID="
-        + st.secrets["username"]
-        + ";PWD="
-        + st.secrets["password"]
-    )
 
-#conn = init_connection()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
 
-rows = run_query("SELECT TOP 10 * FROM new_all_account_all_channel;")
+rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
 
 # Print results.
+st.write("Some wise words from Shakespeare:")
 for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
-
+    st.write("✍️ " + row['word'])
 
