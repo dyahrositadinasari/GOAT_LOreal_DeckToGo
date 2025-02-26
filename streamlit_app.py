6a561d56ed2c88844b0dee03a1a3b35b55b3585a
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 17:17:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+st.title("GOAT L'Oreal PPT Report Automation")
+st.write("Please select the report type")
+
 #---PAGE SET-UP---
 pages = {
     "Monthly Reporting L'Oreal": {
