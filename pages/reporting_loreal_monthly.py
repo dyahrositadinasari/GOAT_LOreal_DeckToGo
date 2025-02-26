2be66b205eac4552be56a7db97f5a948a46ddda2
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 17:45:26 +0700

    Update reporting_loreal_monthly.py

--- pages/reporting_loreal_monthly.py
+++ pages/reporting_loreal_monthly.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("GOAT-L'Oreal Monthly Report")
+st.title("GOAT-LÓreal Monthly Report")
 year = st.selectbox(
   'Please select the reporting year',
   ('2024', '2025')
