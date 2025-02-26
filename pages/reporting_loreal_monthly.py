767fbe2f099057b28aee444a2100b27a7638eaf1
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 17:34:27 +0700

    Update reporting_loreal_monthly.py

--- pages/reporting_loreal_monthly.py
+++ pages/reporting_loreal_monthly.py
@@ -2,14 +2,18 @@ import streamlit as st
 
 st.title("GOAT-L'Oreal Monthly Report")
 year = st.selectbox(
-  'Please select the reporting month',
-  ('2024', '2025', '2026')
+  'Please select the reporting year :',
+  ('2024', '2025')
 )
 month = st.selectbox(
-  'Please select the reporting month',
+  'Please select the reporting month :',
   ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
 )
-st.write(
-  "Year selected:", year,
-  "\nMonth selected:", month
-        )
+division = st.selectbox(
+  'Please select the reporting L'Oreal Division :',
+  ('CPD', 'LDB', 'LLD', 'PPD')
+)
+category = st.selectbox(
+  'Please select the reporting L'Oreal TDK Category :',
+  ('Hair Care', 'Female Skin', 'Make Up', 'Fragrance', 'Men Skin', 'Hair Color')
+)
