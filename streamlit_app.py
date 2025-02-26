e2fa3b57a5f0ad67a0ed8a901221bc080b408c79
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 16:53:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,19 +9,24 @@ with st.sidebar:
     st.write("GOAT L'Oreal Monthly Report")
 
 #---PAGE SET-UP---
-monthly_page = st.page(
+m_page = st.page(
     page ="pages/reporting_loreal_monthly.py",
     title ="Monthly Reporting L'Oreal"
     icon =":bar_chart:",
 )
-quarterly_page = st.page(
+q_page = st.page(
     page ="pages/reporting_loreal_quarterly.py",
     title ="Quarter Reporting L'Oreal"
     icon =":chart_with_downwards_trend:",
 )
-annually_page = st.page(
+a_page = st.page(
     page ="pages/reporting_loreal_yearly.py",
     title ="Annual Reporting L'Oreal"
     icon =":chart_with_upwards_trend:",
 )
 
+#---NAVIGATION SET-UP---
+pg = st.navigation(pages=[m_page, q_page, y_page])
+
+#---RUN NAVIGATION---
+pg.run()
