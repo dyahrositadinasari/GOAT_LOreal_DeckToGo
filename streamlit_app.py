76f45749968108eaccd737723e6dcc2832a436b3
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 17:00:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,18 @@ import streamlit as st
 #---PAGE SET-UP---
 m_page = st.Page(
     page ="pages/reporting_loreal_monthly.py",
-    title ="Monthly Reporting L'Oreal"
+    title ="Monthly Reporting L'Oreal",
     icon =":bar_chart:",
     default =True
 )
 q_page = st.Page(
     page ="pages/reporting_loreal_quarterly.py",
-    title ="Quarter Reporting L'Oreal"
+    title ="Quarter Reporting L'Oreal",
     icon =":chart_with_downwards_trend:",
 )
 a_page = st.Page(
     page ="pages/reporting_loreal_yearly.py",
-    title ="Annual Reporting L'Oreal"
+    title ="Annual Reporting L'Oreal",
     icon =":chart_with_upwards_trend:",
 )
 
