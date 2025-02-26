2b41a9ae7ff4c83be694e77e282610f0c66bfaf4
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 17:24:25 +0700

    Update reporting_loreal_monthly.py

--- pages/reporting_loreal_monthly.py
+++ pages/reporting_loreal_monthly.py
@@ -5,8 +5,6 @@ year = st.selectbox(
   'Please select the reporting month',
   ('2024', '2025', '2026')
 )
-
-st.write('Month selected:', month)
 month = st.selectbox(
   'Please select the reporting month',
   ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
