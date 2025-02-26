46fc741f4f24f0c6011713682ae8f10b25e73968
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 16:56:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,17 +2,12 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
-
-st.title("GOAT L'Oreal PPT Report Automation")
-st.write("Please select the report type")
-with st.sidebar:
-    st.write("GOAT L'Oreal Monthly Report")
-
 #---PAGE SET-UP---
 m_page = st.page(
     page ="pages/reporting_loreal_monthly.py",
     title ="Monthly Reporting L'Oreal"
     icon =":bar_chart:",
+    default =True
 )
 q_page = st.page(
     page ="pages/reporting_loreal_quarterly.py",
