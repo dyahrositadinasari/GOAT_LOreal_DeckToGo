2b41c43c8020077c3bf25b66cd6f741d0661d5a8
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 17:04:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,25 @@
 import streamlit as st
 
 #---PAGE SET-UP---
-m_page = st.Page(
-    page ="pages/reporting_loreal_monthly.py",
-    title ="Monthly Reporting L'Oreal",
-    icon =":bar_chart:",
-    default =True
-)
-q_page = st.Page(
-    page ="pages/reporting_loreal_quarterly.py",
-    title ="Quarter Reporting L'Oreal",
-    icon =":chart_with_downwards_trend:",
-)
-a_page = st.Page(
-    page ="pages/reporting_loreal_yearly.py",
-    title ="Annual Reporting L'Oreal",
-    icon =":chart_with_upwards_trend:",
-)
+pages = {
+    "Monthly Reporting L'Oreal": {
+        "file": "pages/reporting_loreal_monthly.py",
+        "icon": ":bar_chart:"
+    },
+    "Quarter Reporting L'Oreal": {
+        "file": "pages/reporting_loreal_quarterly.py",
+        "icon": ":chart_with_downwards_trend:"
+    },
+    "Annual Reporting L'Oreal": {
+        "file": "pages/reporting_loreal_yearly.py",
+        "icon": ":chart_with_upwards_trend:"
+    }
+}
 
 #---NAVIGATION SET-UP---
-pg = st.navigation(pages=[m_page, q_page, a_page])
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", list(pages.keys()))
 
 #---RUN NAVIGATION---
-pg.run()
+selected_page = pages[selection]["file"]
+exec(open(selected_page).read())
