8cb551dec56136721f733e2eeea8523ad041c937
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 19:40:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 
 st.title("GOAT L'Oreal PPT Report Automation")
 st.write("Please select the report type")
