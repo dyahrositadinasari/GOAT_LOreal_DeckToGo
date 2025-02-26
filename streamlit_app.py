61961529855389cc2f73ce1c9083b6daf0d8e3af
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-26 16:49:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,42 +2,26 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
-
 
 st.title("GOAT L'Oreal PPT Report Automation")
 st.write("Please select the report type")
 with st.sidebar:
     st.write("GOAT L'Oreal Monthly Report")
 
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-rows = run_query("""
-SELECT 
-[Brand]
-,SUM([Views]) as [Views]
-,SUM([Engagement]) as [Engagement]
-FROM loreal-id-prod.loreal_storage.advocacy_tdk
-WHERE [TDK Category] = 'Female Skin'
-AND MONTH([Date]) = '11'
-AND YEAR([Date]) = '2024'
-GROUP BY
-[Brand]
-""")
-
-# Print results.
-st.write(rows)
+#---PAGE SET-UP---
+monthly_page = st.page(
+    page ="pages/reporting_loreal_monthly.py",
+    title ="Monthly Reporting L'Oreal"
+    icon =":bar_chart:",
+)
+quarterly_page = st.page(
+    page ="pages/reporting_loreal_quarterly.py",
+    title ="Quarter Reporting L'Oreal"
+    icon =":chart_with_downwards_trend:",
+)
+annually_page = st.page(
+    page ="pages/reporting_loreal_yearly.py",
+    title ="Annual Reporting L'Oreal"
+    icon =":chart_with_upwards_trend:",
+)
 
