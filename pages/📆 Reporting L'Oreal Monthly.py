e44787b8f888f4b6319179c188e9c1be4268e099
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-27 15:29:46 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -10,9 +10,9 @@ month = st.selectbox(
   ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
 )
 month_map = {
-    "January": 1, "February": 2, "March": 3, "April": 4,
-    "May": 5, "June": 6, "July": 7, "August": 8,
-    "September": 9, "October": 10, "November": 11, "December": 12
+    "January": "1", "February": "2", "March": "3", "April": "4",
+    "May": "5", "June": "6", "July": "7", "August": "8",
+    "September": "9", "October": "10", "November": "11", "December": "12"
 }
 
 month_num = month_map.get(month, "")  # Returns '' if month is not found
