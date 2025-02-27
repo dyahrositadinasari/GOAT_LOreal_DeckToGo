c8836ddf9f5d97018c1d0b2aab5a50cd2d0a15de
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-02-27 14:18:55 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -18,15 +18,15 @@ month_map = {
 month_num = month_map.get(month, "")  # Returns '' if month is not found
 
 division = st.selectbox(
-  "Please select the reporting LÓreal Division",
+  "Please select the reporting L'Oreal Division",
   ('CPD', 'LDB', 'LLD', 'PPD')
 )
 category = st.selectbox(
-  "Please select the reporting LÓreal TDK Category",
+  "Please select the reporting L'Oreal TDK Category",
   ('Hair Care', 'Female Skin', 'Make Up', 'Fragrance', 'Men Skin', 'Hair Color')
 )
 brands = st.multiselect(
-    "Please Select 3 LÓreal Brands to compare in the report",
+    "Please Select 3 L'Oreal Brands to compare in the report",
     ["BLP Skin", "Garnier", "L'Oreal Paris", "GMN Shampoo Color", "Armani", "Kiehls", "Lancome", "Shu Uemura", "Urban Decay", "YSL", "Cerave", "La Roche Posay", "L'Oreal Professionel", "Matrix", "Biolage", "Kerastase", "Maybelline"]
 ,max_selections=3
 )
