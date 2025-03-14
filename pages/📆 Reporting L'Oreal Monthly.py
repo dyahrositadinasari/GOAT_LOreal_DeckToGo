9a5bf5e001fbf9a4b6b22385d9a1c9518cfac790
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-03-14 14:31:55 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -455,7 +455,7 @@ date
 ,SUM(engagements) as engagements
 ,SUM(content) as content
 FROM loreal-id-prod.loreal_storage.advocacy_tdk_df
-WHERE MONTH(date) = year
+WHERE YEAR(date) = year
 GROUP BY 
 date
 ,month
