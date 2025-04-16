0ae961fb169886a0b0d5f02ae6bdb0337a614900
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-16 13:13:10 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -26,7 +26,7 @@ st.logo(
     "image/goat_logo.png", size="medium", link=None, icon_image=None
 )
 st.image("image/Capture.png")
-st.title("GOAT 🔥 L'Oreal Monthly Report")
+st.subtitle("GOAT 🔥 L'Oreal Monthly Report")
 year = st.selectbox(
   'Please select the reporting year',
   ('2024', '2025')
