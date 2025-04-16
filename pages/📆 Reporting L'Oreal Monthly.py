dd75d2b2ab72e67e5456f24e411bd850462e17f2
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-16 13:37:01 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -25,7 +25,7 @@ from pptx.enum.text import MSO_ANCHOR
 st.logo(
     "image/goat_logo.png", size="medium", link=None, icon_image=None
 )
-st.image("image/goathead.png")
+st.image("image/goat-agency-logo.png")
 st.header("GOAT 🔥 L'Oreal Monthly Report")
 year = st.selectbox(
   'Please select the reporting year',
