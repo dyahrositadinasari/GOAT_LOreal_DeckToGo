498396b5e349839648a4a0e8e6364187f099eedc
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-16 15:27:28 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -843,7 +843,7 @@ if st.button("Generate Report", type="primary"):
 	files = ppt.save(filename)
 
 # ✅ This code runs **only after clicking the submit button**
-	st.success(f"✅ Ready to send report: {category_title} Monthly Report - {month} {year}")
+	st.success(f"✅ Your report is ready : {category_title} Monthly Report - {month} {year}")
 
 	# Store filename in session state for later use
 	st.session_state["report_filename"] = filename
