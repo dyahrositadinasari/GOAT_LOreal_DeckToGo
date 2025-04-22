c7384e87f0fb22c46711a4e2a9e17b964bd0c0de
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-22 18:02:34 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -350,6 +350,70 @@ if st.button("Generate Report", type="primary"):
 	
 		return chart
 
+	def vertical_bar_chart(slide, df, x, y, cx, cy, legend=True, legend_position=XL_LEGEND_POSITION.RIGHT,
+							 data_show=True, chart_title=False, title="", fontsize=Pt(12),
+							 fontsize_title=Pt(14), percentage=False, bar_width=Pt(10)):
+		df.fillna(0, inplace=True)  # Fill NaN values
+		# Define chart data
+		chart_data = CategoryChartData()
+		for i in df.index:
+			chart_data.add_category(i)
+		for col in df.columns:
+			chart_data.add_series(col, np.where(df[col].values == 0, None, df[col].values))
+	
+	    # Create vertical bar chart
+		chart = slide.shapes.add_chart(XL_CHART_TYPE.COLUMN_CLUSTERED, x, y, cx, cy, chart_data).chart
+	
+	    # Add legend
+		if legend:
+			chart.has_legend = True
+			chart.legend.include_in_layout = False
+			chart.legend.position = legend_position
+			chart.legend.font.size = fontsize
+		else:
+			chart.has_legend = False
+	
+	    # Show data labels
+		if data_show:
+			for series in chart.series:
+				for point in series.points:
+					point.data_label.show_value = True
+					point.data_label.font.size = fontsize
+					point.data_label.position = XL_LABEL_POSITION.OUTSIDE_END
+	
+	    # Customize category axis (y-axis) label font size
+		chart.category_axis.tick_labels.font.size = fontsize
+	
+	    # Customize value axis (x-axis) label font size and format
+		chart.value_axis.tick_labels.font.size = fontsize
+		if percentage:
+			chart.value_axis.tick_labels.number_format = '0%'
+		elif df.max().max() >= 1000:
+			chart.value_axis.tick_labels.number_format = '#,##0'  # Add commas for thousands
+	
+	    # Adjust bar width
+		for series in chart.series:
+			series.format.line.width = bar_width
+	
+		# Set chart title
+		if chart_title:
+			chart.chart_title.text_frame.text = title
+			title_font = chart.chart_title.text_frame.paragraphs[0].font
+			title_font.bold = True
+			title_font.size = fontsize_title
+		else:
+			chart.has_title = False
+	
+	    # Remove Gridlines
+		value_axis = chart.value_axis
+		category_axis = chart.category_axis
+		value_axis.has_major_gridlines = False
+		value_axis.has_minor_gridlines = False
+		category_axis.has_major_gridlines = False
+		category_axis.has_minor_gridlines = False
+	
+		return chart
+
 	def combo_chart(slide, df, x, y, cx, cy, legend=True, legend_position=XL_LEGEND_POSITION.TOP,
                 data_show=False, chart_title=False, title="", fontsize=Pt(10), fontsize_title=Pt(12),
                 line_width=Pt(1), smooth=False, font_name='Neue Haas Grotesk Text Pro'):
@@ -808,6 +872,13 @@ if st.button("Generate Report", type="primary"):
 	page_no = page_no + 1
 	format_title(ppt.slides[page_no], "MONTHLY PER BRAND CONTRIBUTION", alignment=PP_ALIGN.LEFT, font_name= 'Neue Haas Grotesk Text Pro', font_size=28, font_bold=True,left=Inches(0.5), top=Inches(0.5), width=Inches(12.3), height=Inches(0.3), font_color=RGBColor(0, 0, 0))
 
+	# Filter the dataframe
+	df_10 = df[(df['division'].isin(division)) & (df['category'].isin(category)) & (df['brand'].isin(brands)) & (df['years'] == year_map) &  (df['month'] == month)]
+	
+	df_10_pivot = pd.pivot_table(df_10[['brand', 'views']], index = 'brand', aggfunc = 'sum', fill_value = 0)
+	df_10_pivot
+
+	
 #------------PAGE11--------------
 	page_no = page_no + 1 
 	format_title(ppt.slides[page_no], "QUARTERLY PER BRAND CONTRIBUTION", alignment=PP_ALIGN.LEFT, font_name= 'Neue Haas Grotesk Text Pro', font_size=28, font_bold=True,left=Inches(0.5), top=Inches(0.5), width=Inches(12.3), height=Inches(0.3), font_color=RGBColor(0, 0, 0))
