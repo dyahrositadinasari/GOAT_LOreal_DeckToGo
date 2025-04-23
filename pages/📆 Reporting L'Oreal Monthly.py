81da9c6f4dcb7b34bf37921d77df118ac1af09e2
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-23 15:02:39 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -1004,7 +1004,7 @@ if st.button("Generate Report", type="primary"):
 	format_title(ppt.slides[page_no], category_title.upper()+" KOL MIX", alignment=PP_ALIGN.LEFT, font_name= 'Neue Haas Grotesk Text Pro', font_size=28, font_bold=True,left=Inches(0.5), top=Inches(0.5), width=Inches(12.3), height=Inches(0.3), font_color=RGBColor(0, 0, 0))
 
 	# Filter the dataframe
-	df_14 = df2[(df2['category'].isin(category) & (df2['years'] == year) &  (df2['month'] == month)]
+	df_14 = df2[(df2['category'].isin(category)) & (df2['years'] == year) &  (df2['month'] == month)]
 	df_14_views = pd.pivot_table(df_14[['tier', 'views']], index = 'tier', aggfunc = 'sum', fill_value = 0)
 	st.write("df_14_views :", df_14_views)
 
