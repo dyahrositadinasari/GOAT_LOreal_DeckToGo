7ae66d48bd48d834b4d8f55e8f6e0163fe39fd72
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-24 18:19:43 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -1110,7 +1110,7 @@ if st.button("Generate Report", type="primary"):
 	df_12 = df_12.reset_index()
 	df_12['eng_rate'] = np.where(df_12['views'] != 0, df_12['engagements'] / df_12['views'], 0)
 	df_12['CPV'] =  np.where(df_12['views'] != 0, df_12['rate'] / df_12['views'], 0)
-	df_14_[['rate', 'views', 'engagements', 'content',  'CPV', 'eng_rate']]
+	df_12[['rate', 'views', 'engagements', 'content',  'CPV', 'eng_rate']]
 	df_12_transpose = df_12.transpose()
 	
 	st.write("df_11 :", df_11)
