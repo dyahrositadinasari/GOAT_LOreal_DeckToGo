cfbe9c1df4028d7933f821fd885a05c908b96d85
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-25 13:53:50 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -109,15 +109,6 @@ else:
 #st.write("Email list : ", email_list)
 
 if st.button("Generate Report", type="primary"):
-	#------------PROGRESS------------
-	progress_text = "Operation in progress. Please wait."
-	my_bar = st.progress(0, text=progress_text)
-
-	for percent_complete in range(100):
-		time.sleep(0.01)
-		my_bar.progress(percent_complete + 1, text=progress_text)
-		time.sleep(1)
-		my_bar.empty()
 		
 #--- DATA PROCESSING ---
 
