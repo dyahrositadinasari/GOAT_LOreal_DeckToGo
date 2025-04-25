6cc22411e0dd8d4e138751b2fad6a410724cbdb0
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-25 14:18:20 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -91,8 +91,8 @@ email_list = st.multiselect(
 	)
 
 st.badge(" Optional", icon="⚠️", color="blue")
-st.info("You can use different PPT Template for this report, make sure the page is 20 pages or more.")
-uploaded_file = st.file_uploader(type=["pptx"])
+st.info("You can upload PPT Template for this report, make sure the page is 20 pages or more.")
+uploaded_file = st.file_uploader("", type=["pptx"])
 if uploaded_file is not None:
 	st.success("File uploaded successfully!")
 else:
