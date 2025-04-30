55809b964e24d931fafccb6b4af8449ce9e8cbe0
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-04-30 16:07:20 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -1258,6 +1258,16 @@ if st.button("Generate Report", type="primary"):
                      chart_title = True, title= "SOC", fontsize_title = Pt(16),
                      legend=False, bar_width = Pt(8), percentage=False, fontsize=Pt(10))
 
+	# Add Total Views, Total Eng, Total Content
+	format_title(ppt.slides[page_no], "Views", alignment=PP_ALIGN.CENTER, font_name= 'Poppins', font_size=10.5,left=Inches(4), top=Inches(6.5), width=Inches(0.8), height=Inches(0.6), font_color=RGBColor(0, 0, 0))
+	format_title(ppt.slides[page_no], format(total_views_y, ","), alignment=PP_ALIGN.CENTER, font_name= ''Poppins'', font_size=18, font_bold=True,left=Inches(4), top=Inches(6.2), width=Inches(0.8), height=Inches(0.6), font_color=RGBColor(146, 39, 143))
+
+	format_title(ppt.slides[page_no], "Engagements", alignment=PP_ALIGN.CENTER, font_name= 'Poppins', font_size=10.5, left=Inches(8), top=Inches(6.5), width=Inches(1.3), height=Inches(0.6), font_color=RGBColor(0, 0, 0))
+	format_title(ppt.slides[page_no], format(total_engagement_y, ","), alignment=PP_ALIGN.CENTER, font_name= ''Poppins'', font_size=18, font_bold=True,left=Inches(8), top=Inches(6.2), width=Inches(1.3), height=Inches(0.6), font_color=RGBColor(146, 39, 143))
+
+	format_title(ppt.slides[page_no], "Contents", alignment=PP_ALIGN.CENTER, font_name= 'Poppins', font_size=10.5, font_italic=False,left=Inches(12), top=Inches(6.5), width=Inches(1), height=Inches(0.6), font_color=RGBColor(0, 0, 0))
+	format_title(ppt.slides[page_no], format(total_content_y, ","), alignment=PP_ALIGN.CENTER, font_name= 'Poppins', font_size=18, font_bold=True,left=Inches(12), top=Inches(6.2), width=Inches(1), height=Inches(0.6), font_color=RGBColor(146, 39, 143))
+
 	st.write("Slide 13 of 17")
 	
 #-----------PAGE 14---------------
