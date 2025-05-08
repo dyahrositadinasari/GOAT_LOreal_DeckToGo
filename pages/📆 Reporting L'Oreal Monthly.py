7511d546c151e10baad2dafb5e03cae470070cba
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-05-08 13:56:17 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -1364,7 +1364,7 @@ if st.button("Generate Report", type="primary"):
 	page_no = page_no + 1	
 	format_title(ppt.slides[page_no], "WINNING FORMULA FOR BOOSTED CONTENTS ", alignment=PP_ALIGN.LEFT, font_name= 'Neue Haas Grotesk Text Pro', font_size=28, font_bold=True,left=Inches(0.5), top=Inches(0.5), width=Inches(12.3), height=Inches(0.3), font_color=RGBColor(0, 0, 0))
 	df_17 = df_15[['division', 'campaign', 'link_post', 'kol_name', 'kol_persona',  'views']]
-	df_17 = df_17[(df_17['spark_ads']== 'Boosted')]
+	df_17 = df_17[(df_17['advocacy']== 'Boosted')]
 	df_17 = df_17.sort_values('views', ascending=False).head(6)
 	df_17_transpose = df_17.transpose()
 	df_17_transpose.reset_index(inplace=True)
