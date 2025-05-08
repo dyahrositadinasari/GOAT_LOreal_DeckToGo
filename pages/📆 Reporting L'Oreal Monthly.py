29716012da02403354dfdcd120dc7c74edaa39e4
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-05-08 13:13:40 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -1337,7 +1337,7 @@ if st.button("Generate Report", type="primary"):
 	st.write("df_16_transpose.iloc[:, 1]", df_16_transpose.iloc[:, 1])
 
 	table_default(ppt.slides[page_no], df_16_transpose.iloc[:, 1], Inches(3.5), Inches(6), Inches(2), Inches(1),
-								[Inches(2)]+[Inches(1)], Inches(0.5), header=True, upper=True, fontsize=9, alignment=PP_ALIGN.CENTRE)
+								[Inches(2)]+[Inches(1)], Inches(0.5), header=True, upper=True, fontsize=9, alignment=PP_ALIGN.CENTER)
 
 
 	st.write("Slide 16 of 17 - still in development process", df_16_transpose)
