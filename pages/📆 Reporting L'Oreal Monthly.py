96dd621ee4ca79b1bc2e797407c4a3d89e59af39
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-05-15 14:50:22 +0700

    Update 📆 Reporting L'Oreal Monthly.py

--- pages/📆 Reporting L'Oreal Monthly.py
+++ pages/📆 Reporting L'Oreal Monthly.py
@@ -110,16 +110,7 @@ if uploaded_file is not None:
 	st.success("File uploaded successfully!")
 else:
 	uploaded_file = "pages/Template Deck to Go - L'Oreal Indonesia.pptx"
-
-#st.write("Selected Year : ", year)
-#st.write("Selected Year_ : ", year_map)
-#st.write("Selected Month : ", month)
-#st.write("Month Number : ", month_num)
-#st.write("Division : ", division)
-#st.write("Category : ", category)
-#st.write("Brands : ", brands)
-#st.write("Email list : ", email_list)
-
+	
 if st.button("Generate Report", type="primary"):
 
 #--- DATA PROCESSING ---
